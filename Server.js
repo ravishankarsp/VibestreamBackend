a6eb888b4b7dc4058d227297b03486d000e0e15b
Ravishankar S P <sathiyaravi2001@gmail.com>
2026-04-18 21:44:11 +0530

    updated server.js

--- Server.js
+++ Server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL.split(","),
   }),
 );
 app.use("/api", Route);
