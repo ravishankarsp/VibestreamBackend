0837245a0223bfe887e35d0c5ad9087d521e69b7
Ravishankar S P <sathiyaravi2001@gmail.com>
2026-04-20 16:05:33 +0530

    added 10 more songs

--- route/Route.js
+++ route/Route.js
@@ -2,10 +2,24 @@ import express from "express";
 
 const Route = express.Router();
 
+const HID = [
+  "6LD30ChPsSs",
+  "b68HETiNO98",
+  "oLgzs8nut3A",
+  "kw6SFbbinnY",
+  "2qCpY38ompo",
+  "A04WawrDblo",
+  "tOM-nWPcR4U",
+  "Vijx5tRBCeg",
+  "QcQpqWhTBCE",
+  "88iypMO9H7g",
+  "OViH68fJUhM",
+];
+
 Route.get("/happy", async (req, res) => {
   try {
     let data = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=UIuBtl4rz0E,KgayxOF4Y7E&key=${process.env.YOU_API_KEY}`,
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${shuffleArray(HID)}&key=${process.env.YOU_API_KEY}`,
     );
     let response = await data.json();
     return res.json(response);
@@ -15,3 +29,11 @@ Route.get("/happy", async (req, res) => {
 });
 
 export default Route;
+
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
